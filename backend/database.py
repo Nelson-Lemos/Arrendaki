c375a06a3df8e3639496a9357c6b916539e63f53
Nelson Lemos <nelsoncariamba@gmail.com>
2026-06-25 09:05:35 +0100

    Switch to PostgreSQL + Neon

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 from backend.config import DATABASE_URL
 
 engine = create_engine(DATABASE_URL, echo=False)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
+Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
